29a3b3d891b1e779bd6dd4a35e99c7635554e1f2
butolin-pawel <difenteti@gmail.com>
2023-02-21 16:06:52 +0300

    fn

--- isn-app/src/main.js
+++ isn-app/src/main.js
@@ -1,9 +1,22 @@
-import Vue from 'vue'
+// import Vue from 'vue'
+// import App from './App.vue'
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
+// Vue.config.productionTip = false
+// Vue.use(Vuetify)
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+import { createApp } from 'vue'
 import App from './App.vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-Vue.config.productionTip = false
-Vue.use(Vuetify)
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  
+createApp(App).use(vuetify).mount('#app')
