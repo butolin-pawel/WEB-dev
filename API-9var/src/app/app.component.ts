dd40ab971d0f46dadad2fa935bfb803659fad74b
butolin-pawel <99872063+butolin-pawel@users.noreply.github.com>
2023-03-07 23:07:04 +0300

    onle card withot many

--- API-9var/src/app/app.component.ts
+++ API-9var/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'API-9var';
+  constructor(private http :HttpClient){
+    this.search();
+  }
+  title = 'Ваше аниме на сегодня';
+  responce : any;
+  fotourl : any = 'http://cf.ppt-online.org/files/slide/n/Ncw71DHnRrBi5ulXo2OSPYKb3AmUIWFQz4dM6q/slide-61.jpg';
+  search() {
+    this.http.get('https://animechan.vercel.app/api/random').
+    subscribe(
+        (response) => {
+      this.responce = response;
+      console.log(this.responce);
+    });
+  }
+
 }
--- API-9var/src/app/app.module.ts
+++ API-9var/src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:      [ BrowserModule, FormsModule,HttpClientModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
