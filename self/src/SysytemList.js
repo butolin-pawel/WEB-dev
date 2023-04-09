2d184fd65a1f4637ec4226bd83fd83d62380604f
butolin-pawel <butolin-pawel@yandex.ru>
2023-04-09 17:18:07 +0300

    ww

--- self/src/SysytemList.js
+++ self/src/SysytemList.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import split from 'react';
 import axios from 'axios';
 
 export default class SystemList extends React.Component {
-  state = {
-    systems: []
-  }
-  start;
-  end;
-  res  = '';
-  outy;
+  ish;
+  res =[];
   componentDidMount() {
     axios.get(`https://new.vyatsu.ru/sveden/common`)
         .then(res => {
-            const systems = res.statusText;
-            this.setState({systems});
-          
+            const tp = res.data;
+            this.ish = tp;
+            console.log(this.ish);
         })
+        
+        
   }
-
+  findAddresses() {
+    const regex = /\d{6},/ //\w+ \w+, г. \w+, \w+. \w+, \w. \d{1,2,3}\d\S+/;
+    let match;  
+    while ((match = regex.exec(this.ish))) {
+      this.res.push(match);
+    }
+  }
   render() {
     return (
-      <div>
-          {this.state.systems}
+      <div> 
+        {this.ish}
+        
+          
       </div>
     )
   }
